f7310b7f843a7868637116e3fd01cc2159c89277
Lars Eidam <larseidam@googlemail.com>
2013-01-10 12:57:16 +0100

    rewrite PubsubControllerTest and TestHelper
    test for subscription action

--- tests/PubsubControllerTest.php
+++ tests/PubsubControllerTest.php
@@ -1,26 +1,11 @@
 <?php
-
 require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'TestHelper.php';
 
-require_once '../HubSubscriptionModel.php';
-
-// This constant will not be defined iff this file is executed directly.
-if (!defined('PHPUnit_MAIN_METHOD')) {
-    define('PHPUnit_MAIN_METHOD', 'PubsubControllerTest::main');
-}
-
+/**
+ * 
+ */
 class PubsubControllerTest extends Zend_Test_PHPUnit_ControllerTestCase
 {
-    /**
-     * The main method, which executes all tests inside this class.
-     * 
-     * @return void
-     */
-    public static function main()
-    {
-        PHPUnit_TextUI_TestRunner::run(new ReflectionClass('PubsubControllerTest'));
-    }
-
     public function setUp()
     {
         $this->bootstrap = new Zend_Application(
@@ -30,282 +15,53 @@ class PubsubControllerTest extends Zend_Test_PHPUnit_ControllerTestCase
         parent::setUp();
     }
 
-    public function testCallWithoutAction()
+    /**
+     * @test
+     */
+    public function subscription()
     {
-        $this->dispatch('/pubsub');
+        $this->request
+            ->setPost(
+                array(
+                    'hubUrl' => 'http://localhost:8080/subscribe',
+                    'topicUrl' => 'http://localhost:8081/atom/stream/eugene',
+                    'callBackUrl' => 'http://localhost:8081/callback',
+                    'subscriptionMode' => 'subscribe'
+                )
+            );
 
-        // We expect the error controller with error action here, since ther is
-        // no default index action for this controller.
-        $this->assertController('error');
-        $this->assertAction('error');
-    }
-
-    public function testTestAction()
-    {
-        $this->dispatch('/pubsub/test');
-
-        $this->assertController('pubsub');
-        $this->assertAction('test');
+        // Excute request  
+        $this->dispatch('/pubsub/subscription');
+        
+        //var_dump($this->response);
+        
+        // Check response
         $this->assertResponseCode(200);
-
-        $this->assertQueryContentContains('body', 'PubsubController is enabled');
+        
     }
-
-    public function testHubsubscriptionActionGet()
+    
+    /**
+     * @test
+     */
+    public function unSubscription()
     {
-        $this->dispatch('/pubsub/hubsubscription');
+        $this->request
+            ->setPost(
+                array(
+                    'hubUrl' => 'http://localhost:8080/subscribe',
+                    'topicUrl' => 'http://localhost:8081/atom/stream/eugene',
+                    'callBackUrl' => 'http://localhost:8081/callback',
+                    'subscriptionMode' => 'unsubscribe'
+                )
+            );
 
-        $this->assertController('error');
-        $this->assertAction('error');
-        $this->assertResponseCode(400);        
+        // Excute request  
+        $this->dispatch('/pubsub/subscription');
+        
+        //var_dump($this->response);
+        
+        // Check response
+        $this->assertResponseCode(200);
+        
     }
-
-    public function testHubsubscriptionActionPostNoParams()
-    {
-        $this->request->setMethod('POST');
-
-        $this->dispatch('/pubsub/hubsubscription');
-        $this->assertController('error');
-        $this->assertAction('error');
-        $this->assertResponseCode(400);
-    }
-
-    public function testHubsubscriptionActionPostInvalidCallback()
-    {
-        $this->request->setMethod('POST');
-
-        $this->_request->setParams(array(
-            'hub.callback' => 'http://example.org/pubsub/callback#fragment'
-        ));
-
-        $this->dispatch('/pubsub/hubsubscription');
-        $this->assertController('error');
-        $this->assertAction('error');
-        $this->assertResponseCode(400);
-    }
-
-    public function testHubsubscriptionActionPostInvalidMode()
-    {
-        $this->request->setMethod('POST');
-
-        $this->_request->setParams(array(
-            'hub.mode' => 'invalidHubMode'
-        ));
-
-        $this->dispatch('/pubsub/hubsubscription');
-        $this->assertController('error');
-        $this->assertAction('error');
-        $this->assertResponseCode(400);
-    }
-
-    public function testHubsubscriptionActionPostInvalidTopic()
-    {
-        $this->request->setMethod('POST');
-
-        $this->_request->setParams(array(
-            'hub.topic' => 'http://example.org/content/topic#fragment'
-        ));
-
-        $this->dispatch('/pubsub/hubsubscription');
-        $this->assertController('error');
-        $this->assertAction('error');
-        $this->assertResponseCode(400);
-    }
-
-    public function testHubsubscriptionActionPostInvalidVerify()
-    {
-        $this->request->setMethod('POST');
-        $this->_request->setPost(array(
-            'hub.verify' => 'invalidVerifyParam'
-        ));
-
-        $this->dispatch('/pubsub/hubsubscription');
-        $this->assertController('error');
-        $this->assertAction('error');
-        $this->assertResponseCode(400);
-    }
-
-    public function testHubsubscriptionActionPostSubscribeAlreadySubscribed()
-    {
-        $callback = 'http://exmaple.org/callback';
-        $topic = 'http://example.org/topic';
-        $mode = 'subscribe';
-        $verify = 'sync';
-
-        HubSubscriptionModel::$testMode = true;
-        HubSubscriptionModel::$testData = array();
-        $hubModel = new HubSubscriptionModel();
-        $hubModel->addSubscription(array(
-            'hub.callback'  => $callback,
-            'hub.topic'     => $topic,
-            'hub.mode'      => $mode,
-            'hub.verify'    => $verify,
-            'hub.challenge' => md5('Test')
-        ));
-
-        $this->request->setMethod('POST');
-        $this->_request->setPost(array(
-            'hub.callback'  => $callback,
-            'hub.topic'     => $topic,
-            'hub.mode'      => $mode,
-            'hub.verify'    => $verify
-        ));
-
-        $this->dispatch('/pubsub/hubsubscription');
-        $this->assertController('error');
-        $this->assertAction('error');
-        $this->assertResponseCode(500, 'Response code is: ' . $this->_response->getHttpResponseCode());
-    }    
-
-    public function testHubsubscriptionActionPostSubscribeSyncVerificationFailNoOK()
-    {
-        $callback = 'http://exmaple.org/callback';
-        $topic = 'http://example.org/topic';
-        $mode = 'subscribe';
-        $verify = 'sync';
-
-        HubSubscriptionModel::$testMode = true;
-        HubSubscriptionModel::$testData = array();
-
-        $adapter = new Zend_Http_Client_Adapter_Test();
-        Erfurt_App::$httpAdapter = $adapter;
-        $adapter->setResponse(new Zend_Http_Response(
-            404, 
-            array(), 
-            'Not Found'
-        ));
-
-        $this->request->setMethod('POST');
-        $this->_request->setPost(array(
-            'hub.callback'  => $callback,
-            'hub.topic'     => $topic,
-            'hub.mode'      => $mode,
-            'hub.verify'    => $verify
-        ));
-
-        $this->dispatch('/pubsub/hubsubscription');
-        $this->assertController('error');
-        $this->assertAction('error');
-        $this->assertResponseCode(500, 'Response code is: ' . $this->_response->getHttpResponseCode());
-    }
-
-    public function testHubsubscriptionActionPostSubscribeSyncVerificationFailWrongPayload()
-    {
-        $callback = 'http://exmaple.org/callback';
-        $topic = 'http://example.org/topic';
-        $mode = 'subscribe';
-        $verify = 'sync';
-
-        HubSubscriptionModel::$testMode = true;
-        HubSubscriptionModel::$testData = array();
-
-        $adapter = new Zend_Http_Client_Adapter_Test();
-        Erfurt_App::$httpAdapter = $adapter;
-        $adapter->setResponse(new Zend_Http_Response(
-            200, 
-            array(), 
-            'ThisIsAWrongChallengeResult'
-        ));
-
-        $this->request->setMethod('POST');
-        $this->_request->setPost(array(
-            'hub.callback'  => $callback,
-            'hub.topic'     => $topic,
-            'hub.mode'      => $mode,
-            'hub.verify'    => $verify
-        ));
-
-        $this->dispatch('/pubsub/hubsubscription');
-        $this->assertController('error');
-        $this->assertAction('error');
-        $this->assertResponseCode(500, 'Response code is: ' . $this->_response->getHttpResponseCode());
-    }
-
-    public function testHubsubscriptionActionPostSubscribeSyncVerificationSuccess204()
-    {
-        if (!defined('PUBSUB_TEST_MODE')) {
-            define('PUBSUB_TEST_MODE', 1);
-        }
-
-        $callback = 'http://exmaple.org/callback';
-        $topic = 'http://example.org/topic';
-        $mode = 'subscribe';
-        $verify = 'sync';
-
-        HubSubscriptionModel::$testMode = true;
-        HubSubscriptionModel::$testData = array();
-
-        $adapter = new Zend_Http_Client_Adapter_Test();
-        Erfurt_App::$httpAdapter = $adapter;
-        $adapter->setResponse(new Zend_Http_Response(
-            200, 
-            array(), 
-            'TestChallenge'
-        ));
-
-        $this->request->setMethod('POST');
-        $this->_request->setPost(array(
-            'hub.callback'  => $callback,
-            'hub.topic'     => $topic,
-            'hub.mode'      => $mode,
-            'hub.verify'    => $verify
-        ));
-
-        $this->dispatch('/pubsub/hubsubscription');
-        $this->assertController('pubsub');
-        $this->assertAction('hubsubscription');
-        $this->assertResponseCode(204, 'Response code is: ' . $this->_response->getHttpResponseCode());
-    }
-
-    public function testHubsubscriptionActionPostSubscribeAsyncVerificationSuccess202()
-    {
-        if (!defined('PUBSUB_TEST_MODE')) {
-            define('PUBSUB_TEST_MODE', 1);
-        }
-
-        $callback = 'http://exmaple.org/callback';
-        $topic = 'http://example.org/topic';
-        $mode = 'subscribe';
-        $verify = 'async';
-
-        HubSubscriptionModel::$testMode = true;
-        HubSubscriptionModel::$testData = array();
-
-        $adapter = new Zend_Http_Client_Adapter_Test();
-        Erfurt_App::$httpAdapter = $adapter;
-        $adapter->setResponse(new Zend_Http_Response(
-            200, 
-            array(), 
-            'TestChallenge'
-        ));
-
-        $this->request->setMethod('POST');
-        $this->_request->setPost(array(
-            'hub.callback'  => $callback,
-            'hub.topic'     => $topic,
-            'hub.mode'      => $mode,
-            'hub.verify'    => $verify
-        ));
-
-        $this->dispatch('/pubsub/hubsubscription');
-        $this->assertController('pubsub');
-        $this->assertAction('hubsubscription');
-        $this->assertResponseCode(202, 'Response code is: ' . $this->_response->getHttpResponseCode());
-    }
-
-    public function testHubperformasyncverifiesActionNoPending()
-    {
-        HubSubscriptionModel::$testMode = true;
-        HubSubscriptionModel::$testData = array();
-
-        $this->dispatch('/pubsub/hubperformasyncverifies');
-        $this->assertController('pubsub');
-        $this->assertAction('hubperformasyncverifies');
-        $this->assertResponseCode(200, 'Response code is: ' . $this->_response->getHttpResponseCode());
-    }
-}
-
-// If this file is executed directly, execute the tests.
-if (PHPUnit_MAIN_METHOD === 'PubsubControllerTest::main') {
-    PubsubControllerTest::main();
 }
--- tests/TestHelper.php
+++ tests/TestHelper.php
@@ -1,9 +1,11 @@
 <?php
 
 define('BOOTSTRAP_FILE', basename(__FILE__));
-define('ONTOWIKI_ROOT', realpath(dirname(dirname(__FILE__)) . '/../..') . '/');
+define('ONTOWIKI_ROOT', realpath(dirname(__FILE__) . '/../../..') . '/');
 define('APPLICATION_PATH', ONTOWIKI_ROOT . 'application/');
+define('APPLICATION_ENV', 'unittesting');
 define('ONTOWIKI_REWRITE', false);
+define('CACHE_PATH', ONTOWIKI_ROOT . 'cache'.DIRECTORY_SEPARATOR);
 
 // path to tests
 define('_TESTROOT', rtrim(dirname(__FILE__), '/') . '/');
@@ -12,11 +14,14 @@ define('_TESTROOT', rtrim(dirname(__FILE__), '/') . '/');
 define('_OWROOT', ONTOWIKI_ROOT);
 
 // add libraries to include path
-$includePath  = get_include_path()                              . PATH_SEPARATOR;
-$includePath .= _TESTROOT                                       . PATH_SEPARATOR;
-$includePath .= ONTOWIKI_ROOT . 'application/classes/'          . PATH_SEPARATOR;
-$includePath .= ONTOWIKI_ROOT . 'libraries/'                    . PATH_SEPARATOR;
-$includePath .= ONTOWIKI_ROOT . 'libraries/Erfurt/'             . PATH_SEPARATOR;
+$includePath  = get_include_path()                                  . PATH_SEPARATOR;
+$includePath .= _TESTROOT                                           . PATH_SEPARATOR;
+$includePath .= ONTOWIKI_ROOT . 'application/classes/'              . PATH_SEPARATOR;
+$includePath .= ONTOWIKI_ROOT . 'libraries/'                        . PATH_SEPARATOR;
+$includePath .= ONTOWIKI_ROOT . 'libraries/Zend/'                   . PATH_SEPARATOR;
+$includePath .= ONTOWIKI_ROOT . 'libraries/Erfurt/library/'         . PATH_SEPARATOR;
+$includePath .= ONTOWIKI_ROOT . 'libraries/Erfurt/tests/unit'       . PATH_SEPARATOR; // for test base class
+$includePath .= __DIR__       . '/../Model/'                        . PATH_SEPARATOR;
 set_include_path($includePath);
 
 // start dummy session before any PHPUnit output
@@ -28,7 +33,9 @@ require_once 'Zend/Loader/Autoloader.php';
 $loader = Zend_Loader_Autoloader::getInstance();
 $loader->registerNamespace('OntoWiki_');
 $loader->registerNamespace('Erfurt_');
+$loader->registerNamespace('Zend_');
 $loader->registerNamespace('PHPUnit_');
+$loader->registerNamespace('PubSubHubbub_');
 
 /** OntoWiki */
 require_once 'OntoWiki.php';
