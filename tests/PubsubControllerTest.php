1c98d0ce565cdd608fecbba2068739410ca28b47
Lars Eidam <larseidam@googlemail.com>
2013-01-11 18:03:10 +0100

    update PubsubController tests
    update subscribe and unsubscribe tests
    add callback test (deaktivated because missing setUp)

--- tests/PubsubControllerTest.php
+++ tests/PubsubControllerTest.php
@@ -12,7 +12,22 @@ class PubsubControllerTest extends Zend_Test_PHPUnit_ControllerTestCase
             'default',
             ONTOWIKI_ROOT . 'application/config/application.ini'
         );
+        
         parent::setUp();
+        
+        $this->request
+            ->setPost(
+                array(
+                    'logintype' => 'locallogin',
+                    'username' => 'Admin',
+                    'password' => '',
+                    'login-save' => 'on'
+                )
+            );
+            
+        // Excute request  
+        $this->dispatch('/application/login');
+        
     }
 
     /**
@@ -20,24 +35,55 @@ class PubsubControllerTest extends Zend_Test_PHPUnit_ControllerTestCase
      */
     public function subscription()
     {
+
+        
         $this->request
             ->setPost(
                 array(
                     'hubUrl' => 'http://localhost:8080/subscribe',
                     'topicUrl' => 'http://localhost:8081/atom/stream/eugene',
-                    'callBackUrl' => 'http://localhost:8081/callback',
-                    'subscriptionMode' => 'subscribe'
+                    'callBackUrl' => 'http://www.pshb.local/pubsub/callback',
+                    'subscriptionMode' => 'subscribe',
+                    'verifyMode' => 'sync'
                 )
             );
 
         // Excute request  
         $this->dispatch('/pubsub/subscription');
         
-        //var_dump($this->response->getBody());
+        // Check response
+        $this->assertResponseCode(
+            200,
+            "" == $this->response->getBody() ? null : $this->response->getBody()
+        );
+        
+    }
+    
+    /**
+     * @1test
+     */
+    public function callback()
+    {
+        $this->request
+            ->setParams(
+                array(
+                    'hub_topic' => 'http://localhost:8081/atom/stream/eugene',
+                    'hub_challenge' => '1111111111111111111111111',
+                    'hub_verify_token' => 'ec01547f7d5daa0f060d5f7210690bb6',
+                    'xhub_subscription' => 'ec01547f7d5daa0f060d5f7210690bb6',
+                    'hub_mode' => 'subscribe',
+                    'hub_lease_seconds' => '5'
+                )
+            );
+
+        // Excute request  
+        $this->dispatch('/pubsub/callback');
         
         // Check response
-        $this->assertResponseCode(200);
-        
+        $this->assertResponseCode(
+            200,
+            "" == $this->response->getBody() ? null : $this->response->getBody()
+        );
     }
     
     /**
@@ -50,18 +96,20 @@ class PubsubControllerTest extends Zend_Test_PHPUnit_ControllerTestCase
                 array(
                     'hubUrl' => 'http://localhost:8080/subscribe',
                     'topicUrl' => 'http://localhost:8081/atom/stream/eugene',
-                    'callBackUrl' => 'http://localhost:8081/callback',
-                    'subscriptionMode' => 'unsubscribe'
+                    'callBackUrl' => 'http://www.pshb.local/pubsub/callback',
+                    'subscriptionMode' => 'unsubscribe',
+                    'verifyMode' => 'sync'
                 )
             );
 
         // Excute request  
         $this->dispatch('/pubsub/subscription');
         
-        //var_dump($this->response->getBody());
-        
         // Check response
-        $this->assertResponseCode(200);
+        $this->assertResponseCode(
+            200,
+            "" == $this->response->getBody() ? null : $this->response->getBody()
+        );
         
     }
 }
