de636183335b69869909be5441f6c9a5f08c1b76
Konrad Abicht <k.abicht@googlemail.com>
2013-02-28 15:41:18 +0100

    Fix #20 (Add Link or X-Link to resource header) using PubsubPlugin event
    - if you open an URI which points to a resource in an OntoWiki
    the event onBeforeLinkedDataRedirect gets fired; we catch this
    event and add "link" to header fields

--- PubsubPlugin.php
+++ PubsubPlugin.php
@@ -9,6 +9,23 @@
 class PubsubPlugin extends OntoWiki_Plugin
 {
     /**
+     * used on linked data redirect to add link field to header
+     */
+    public function onBeforeLinkedDataRedirect($event)
+    {
+        if ($event->response === null) {
+            return;
+        }
+
+        // set history feed url
+        $historyFeed = OntoWiki::getInstance()->config->urlBase . 'history/feed/?r=';
+        $historyFeed .= urlencode((string) OntoWiki::getInstance()->selectedResource);
+        
+        // set header field
+        $event->response->setHeader('link', $historyFeed, true);
+    }
+    
+    /**
      * Trigger on Pub Hub pushed new feed updates for a particular topicUrl.
      * @param $event Erfurt_Event containing xhubSubscription property with subscription id
      */
